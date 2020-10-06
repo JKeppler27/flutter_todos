f8a3cee72ac3de5851eb45fbdb91052bffd75604
Jonathan Keppler <JKeppler27@gmail.com>
2020-10-05 22:09:09 -0400

    finish blocs

---
+++ lib/blocs/bloc/tab_bloc.dart
@@ -0,0 +1,18 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter_todos/models/models.dart';
+
+part 'tab_event.dart';
+
+class TabBloc extends Bloc<TabEvent, AppTab> {
+  TabBloc() : super(AppTab.todos);
+
+  @override
+  Stream<AppTab> mapEventToState(TabEvent event) async* {
+    if (event is TabUpdated) {
+      yield event.tab;
+    }
+  }
+}
--- lib/blocs/blocs.dart
+++ lib/blocs/blocs.dart
@@ -1,2 +1,5 @@
 export 'bloc/todos_bloc.dart';
 export 'bloc/filtered_todos_bloc.dart';
+export 'bloc/stats_bloc.dart';
+export 'bloc/tab_bloc.dart';
+export 'bloc/simple_bloc_observer.dart';
